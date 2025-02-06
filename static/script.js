4d03f6de1d64b5a9a5ab849c8db9a2e35c59d155
Mohamed Fouad <162137526+Mohamed9x60@users.noreply.github.com>
2025-02-07 00:58:33 +0200

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,15 @@
+// إظهار زر الرجوع إلى الأعلى عند التمرير
+const backToTopButton = document.getElementById("back-to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+});
+
+// العودة إلى الأعلى عند النقر
+backToTopButton.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
